d2799adfad44055fd3cd36576cb4cc5b02fb8304
Martinochampagne <martinopoloni@gmail.com>
2023-10-11 09:19:34 +0200

    autonoleggio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       
+        Veicolo p = new Veicolo (4324654, 5654, "PANDINO", 4);
+	    System.out.println(p.toString());
     }
 }
